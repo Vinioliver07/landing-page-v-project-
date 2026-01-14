0e38d3abbbc7269e2e84e8d044f80525ad379e31
Vinícius de Oliveira <viniciuss100@hotmail.com>
2026-01-14 00:12:53 -0300

    alnding page v project projeto

---
+++ .next/dev/build/postcss.js
@@ -0,0 +1,6 @@
+var R=require("./chunks/[turbopack]_runtime.js")("postcss.js")
+R.c("chunks/[turbopack-node]_transforms_postcss_ts_c8a4aed4._.js")
+R.c("chunks/[root-of-the-server]__0a6a0dc0._.js")
+R.m("[turbopack-node]/globals.ts [postcss] (ecmascript)")
+R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Desktop/clientes/esperando resposta/landing-page-v-project-/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)")
+module.exports=R.m("[turbopack-node]/ipc/evaluate.ts/evaluate.js { INNER => \"[turbopack-node]/transforms/postcss.ts { CONFIG => \\\"[project]/Desktop/clientes/esperando resposta/landing-page-v-project-/postcss.config.js_.loader.mjs [postcss] (ecmascript)\\\" } [postcss] (ecmascript)\", RUNTIME => \"[turbopack-node]/ipc/evaluate.ts [postcss] (ecmascript)\" } [postcss] (ecmascript)").exports
